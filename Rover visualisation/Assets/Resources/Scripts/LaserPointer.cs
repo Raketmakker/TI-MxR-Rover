d6b4321703392b669e07aa5464aa1dde493c40b1
Jasper <j.bonefaas@student.avans.nl>
2021-04-19 14:57:31 +0200

    Added VR buttons with laserpointer

---
+++ Rover visualisation/Assets/Resources/Scripts/LaserPointer.cs
@@ -0,0 +1,37 @@
+﻿/* SceneHandler.cs*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR.Extras;
+
+public class LaserPointer : MonoBehaviour
+{
+    public SteamVR_LaserPointer laserPointer;
+
+    void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += PointerClick;
+    }
+
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        Button b = e.target.GetComponent<Button>();
+        b?.OnSubmit(null);
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+        Button b = e.target.GetComponent<Button>();
+        b?.OnSelect(null);
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        Button b = e.target.GetComponent<Button>();
+        b?.OnDeselect(null);
+    }
+}
