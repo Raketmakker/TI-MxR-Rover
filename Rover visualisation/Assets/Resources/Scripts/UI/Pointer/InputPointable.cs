03a9354f48abb2298aa0eabb5596d3c475cd6ff5
Jasper <j.bonefaas@student.avans.nl>
2021-04-20 15:38:49 +0200

    Added input field and InputPointable

---
+++ Rover visualisation/Assets/Resources/Scripts/UI/Pointer/InputPointable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.Extras;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class InputPointable : MonoBehaviour, IPointable
+{
+    private InputField inputField;
+
+    private void Start()
+    {
+        this.inputField = GetComponent<InputField>();
+    }
+
+    public void OnPointerClick(object sender, PointerEventArgs e)
+    {
+        this.inputField.OnSelect(null);
+    }
+
+    public void OnPointerInside(object sender, PointerEventArgs e)
+    {
+        
+    }
+
+    public void OnPointerOutside(object sender, PointerEventArgs e)
+    {
+        
+    }
+}
