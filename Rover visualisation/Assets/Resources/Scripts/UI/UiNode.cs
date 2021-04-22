157dedaf37cada7652122ce7f3da794432015c5f
Jasper <j.bonefaas@student.avans.nl>
2021-04-22 14:56:09 +0200

    Added mapping mode and settings panel. Changed picture to 2D sprites

---
+++ Rover visualisation/Assets/Resources/Scripts/UI/UiNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiNode : MonoBehaviour
+{
+    public GameObject nodeToDisable;
+    public List<GameObject> nodes = new List<GameObject>();
+
+    public void SelectNode(int nodeIndex)
+    {
+        if (nodeIndex >= this.nodes.Count)
+            return;
+
+        this.nodes[nodeIndex].SetActive(true);
+        this.nodeToDisable.SetActive(false);
+    }
+}
