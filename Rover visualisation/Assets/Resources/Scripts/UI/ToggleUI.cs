ffbe2231f22c67d36640daa9517d3449a9c40708
Jasper <j.bonefaas@student.avans.nl>
2021-04-26 15:03:30 +0200

    Added toggle option for context UI

---
+++ Rover visualisation/Assets/Resources/Scripts/UI/ToggleUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.Extras;
+
+public class ToggleUI : MonoBehaviour
+{
+    public SteamVR_Action_Boolean menuAction;
+    public GameObject userInterface;
+
+    private void Update()
+    {
+        if (this.menuAction.GetStateDown(SteamVR_Input_Sources.Any) || this.menuAction.GetStateUp(SteamVR_Input_Sources.Any))
+        {
+            this.userInterface.SetActive(!this.userInterface.activeInHierarchy);
+        }
+    }
+}
