77527e33490dd2abf7dd1837aeb1f18a95c5a689
Wessel van Zeggeren <wessel.van.zeggeren@gmail.com>
2021-05-10 12:28:46 +0200

    Now filters through the audio and video m3u8 file

--- Rover visualisation/HLS-Test-Module/HLS-Test-Module/HLSStream.cs
+++ Rover visualisation/HLS-Test-Module/HLS-Test-Module/HLSStream.cs
@@ -10,26 +10,32 @@ namespace HLS_Test_Module
     class HLSStream
     {
 
-        public string url;
+        // public parameters
         public string location;
+        public string url;
 
         public string m3u8File;
-        public string segmentName;
-        public string segmentType;
 
         public int m3u8VideoIndex;
 
+        // private parameters
         private string m3u8VideoFile;
         private string m3u8AudioFile;
 
+        private string videoUrl;
+        private string audioUrl;
+
         private int currentVideoSegment;
         private int currentAudioSegment;
+
+        private string segmentName;
+        private string segmentType;
 
         // constructor
         public HLSStream()
         {
 
-            this.m3u8VideoIndex = 0x00;
+            this.m3u8VideoIndex = 0x01;
 
             this.currentVideoSegment = 0x00;
             this.currentAudioSegment = 0x00;
@@ -39,23 +45,28 @@ namespace HLS_Test_Module
         public void start()
         {
 
-            this.downloadFileAsync(this.m3u8File + ".m3u8", new AsyncCompletedEventHandler(m3u8Callback));
+            this.downloadM3u8FileAsync(this.m3u8File, new AsyncCompletedEventHandler(m3u8Callback));
         }
 
-        // download Async file function
-        public void downloadFileAsync(string filename, AsyncCompletedEventHandler callback)
+        // download Async file functions
+        public void downloadM3u8FileAsync(string filename, AsyncCompletedEventHandler callback)
+        {       
+            
+            this.downloadFileAsync(this.url, this.location + "m3u8s\\", filename, callback);
+        }
+        public void downloadFileAsync(string url, string location, string filename, AsyncCompletedEventHandler callback)
         {
 
             using(WebClient WC = new WebClient())
             {
 
                 WC.DownloadFileCompleted += callback;
-                WC.DownloadFileAsync(new System.Uri(this.url + filename), this.location + filename);
+                WC.DownloadFileAsync(new Uri(url + filename), location + filename);
             }
         }
 
         // read the file for valueble lines
-        public List<string> readFile(string location, string filename, string search)
+        public List<string> readFile(string location, string filename, string search = null)
         {
 
             System.IO.StreamReader file = new System.IO.StreamReader(location + filename);
@@ -66,7 +77,7 @@ namespace HLS_Test_Module
             while ((line = file.ReadLine()) != null)
             {
 
-                if (line.Contains(search))
+                if (search == null || line.Contains(search))
                     lines.Add(line);
 
                 counter++;
@@ -75,31 +86,77 @@ namespace HLS_Test_Module
             return lines;
         }
 
+        private void readFileAudioVideo(out string url, out int currentSegment, string filename, string search = null)
+        {
+
+            List<string> lines = this.readFile(this.location + "m3u8s\\", filename, search);
+
+            string[] segments = lines[0].Split('/');
+
+            this.setSegment(segments[segments.Length - 1]);
+            
+            url = this.url + lines[0].Split(this.segmentName)[0];
+            currentSegment = Int32.Parse(lines[lines.Count - 1].Split(this.segmentName)[1].Split(this.segmentType)[0]);
+        }
+
+        private void setSegment(string filename)
+        {
+
+            if (this.segmentName == null || this.segmentType == null)
+            {
+
+                this.segmentName = filename.Split('0')[0];
+                this.segmentType = filename.Split('0')[1];
+
+                Console.WriteLine("segmentName:\t\t" + this.segmentName + "\nsegmentType:\t\t" + this.segmentType);
+            }
+        }
+
         // m3u8 callbacks
         private void m3u8Callback(object sender, AsyncCompletedEventArgs e)
         {
 
-            List<string> lines = this.readFile(this.location, this.m3u8File + ".m3u8", this.m3u8File);
+            Console.WriteLine("Downloaded:\t\t" + this.m3u8File);
+            
+            List<string> lines = this.readFile(this.location + "m3u8s\\", this.m3u8File, this.m3u8File.Split('.')[0]);
 
-            this.m3u8AudioFile = lines[0].Split("URI=\"")[1];
-            this.m3u8AudioFile = this.m3u8AudioFile.Substring(0, this.m3u8AudioFile.Length - 1);
+            this.m3u8AudioFile = lines[0].Split("URI=\"")[1].Split('"')[0];
+            this.m3u8VideoFile = lines[this.m3u8VideoIndex];
 
-            this.m3u8VideoFile = lines[this.m3u8VideoIndex + 1];
+            Console.WriteLine("\nm3u8AudioFile:\t\t" + this.m3u8AudioFile);
 
-            this.downloadFileAsync(this.m3u8AudioFile, new AsyncCompletedEventHandler(m3u8AudioCallback));
-            this.downloadFileAsync(this.m3u8VideoFile, new AsyncCompletedEventHandler(m3u8VideoCallback));
-        }
+            for (int i = 1; i < lines.Count; i++)
+                Console.WriteLine("m3u8VideoIndex " + i + ":\t" + lines[i]);
 
-        private void m3u8VideoCallback(object sender, AsyncCompletedEventArgs e)
-        {
-
-            Console.WriteLine("Downloaded the m3u8 video file");
+            this.downloadM3u8FileAsync(this.m3u8AudioFile, new AsyncCompletedEventHandler(m3u8AudioCallback));
+            this.downloadM3u8FileAsync(this.m3u8VideoFile, new AsyncCompletedEventHandler(m3u8VideoCallback));
         }
 
         private void m3u8AudioCallback(object sender, AsyncCompletedEventArgs e)
         {
 
-            Console.WriteLine("Downloaded the m3u8 audio file");
+            Console.WriteLine("\nDownloaded:\t\t" + this.m3u8AudioFile);
+
+            this.readFileAudioVideo(out this.audioUrl, out this.currentAudioSegment, this.m3u8AudioFile, "audio");
+
+            Console.WriteLine("audioUrl:\t\t" + this.audioUrl);
+        }
+
+        private void m3u8VideoCallback(object sender, AsyncCompletedEventArgs e)
+        {
+
+            Console.WriteLine("\nDownloaded:\t\t" + this.m3u8VideoFile);
+
+            this.readFileAudioVideo(out this.videoUrl, out this.currentVideoSegment, this.m3u8VideoFile, "video");
+
+            Console.WriteLine("videoUrl:\t\t" + this.videoUrl);
+        }
+        
+        // loops
+        private void fileAquireLoop(out int currentSegment, string url, string location)
+        {
+
+            currentSegment = 1;
         }
     }
 }
--- Rover visualisation/HLS-Test-Module/HLS-Test-Module/Program.cs
+++ Rover visualisation/HLS-Test-Module/HLS-Test-Module/Program.cs
@@ -14,11 +14,8 @@ namespace HLS_Test_Module
             HLS.url = "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/";
             HLS.location = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName + "\\HLSFiles\\";
 
-            HLS.m3u8File = "f08e80da-bf1d-4e3d-8899-f0f6155f6efa";
+            HLS.m3u8File = "f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
             HLS.m3u8VideoIndex = 0x05;
-
-            HLS.segmentName = "segment_";
-            HLS.segmentType = ".ts";
 
             HLS.start();
 
