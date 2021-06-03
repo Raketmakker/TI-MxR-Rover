040eb9bef4eaaf0103df970770301c99e42e9e43
jasper <jasper.bonefaas@gmail.com>
2021-06-03 13:36:11 +0200

    Added slider for video parsing percentage

---
+++ Rover visualisation/Assets/Resources/Scripts/UI/SliderController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    public ImageProcessor imageProcessor;
+    public Slider slider;
+
+    private void Awake()
+    {
+        this.imageProcessor.OnImageProgress += OnImageProgress;
+    }
+
+    private void OnImageProgress(object sender, float progress)
+    {
+        this.slider.value = progress;
+    }
+}
